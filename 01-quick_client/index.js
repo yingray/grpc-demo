80b3d25772d1fa97ebfd922379c0cbe4ceccd807
Yingray Lu <yingray_lu@htc.com>
2018-08-16 23:58:37 +0800

    2nd commit

--- 01-quick_client/index.js
+++ 01-quick_client/index.js
@@ -4,7 +4,7 @@ const grpc = require("grpc");
 const protoLoader = require("@grpc/proto-loader");
 const request = require('./request');
 
-const PROTO_PATH = "./assets/index.proto";
+const PROTO_PATH = path.resolve(__dirname, "../proto/index.proto");
 const API_URI = process.env.API_URI || "localhost:8763";
 // const PEM_PATH = path.resolve(__dirname, "./assets/server.pem");
 // const cred = grpc.credentials.createSsl(fs.readFileSync(PEM_PATH));
@@ -24,10 +24,15 @@ const proto = grpc.loadPackageDefinition(def)[packageName];
 const serviceName = Object.keys(proto)[0]
 const client = new proto[serviceName](API_URI, cred);
 
+console.log(`API: ${API_URI}`)
+console.log(`Start making request to ${serviceName} - ${request.name}...`)
+
 client[request.name](request.body, (err, response) => {
   if (err) {
     console.error(err);
     return;
   }
-  return console.log(response);
+  console.log(`Get the response:`)
+  console.log(response);
+  return 
 });
